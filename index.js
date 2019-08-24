33a8bff70e004e7830ce653cef7950b6fa28be03
Martin-Cejka1 <martin.cejka1@ibm.com>
2019-08-24 21:14:39 +0200

    Newlines

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var http = require('http');
 
 var server = http.createServer(function(request, response) {
     
-    response.writeHead(200, {"Content-Type": "text/html; charset=utf-8\n\n"});
+    response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
     
     var url = "https://" + "cs-keyvaultstorage" + ".vault.azure.net/";
 
@@ -22,11 +22,11 @@ var server = http.createServer(function(request, response) {
 
     //var secretUsed = useSecret(secret);
     
-    response.write("MSI SECRET: " + process.env.MSI_SECRET + "\n");
-    response.write("MSI ENDPOINT: " + process.env.MSI_ENDPOINT + "\n");
-    response.write("CLIENT ID: " + process.env.CLIENT_ID + "\n");
-    response.write("MSI_DOMAIN: " + process.env.MSI_DOMAIN + "\n");
-    response.write("Keyvault URL: " + url + "\n");
+    response.write("MSI SECRET: " + process.env.MSI_SECRET + os.EOL);
+    response.write("MSI ENDPOINT: " + process.env.MSI_ENDPOINT + os.EOL);
+    //response.write("CLIENT ID: " + process.env.CLIENT_ID + "\n");
+    //response.write("MSI_DOMAIN: " + process.env.MSI_DOMAIN + "\n");
+    response.write("Keyvault URL: " + url + os.EOL);
     //response.write("credentials: " + credentials + "\n");
     //response.write("credentials as string: " + credentialString + "\n");
     //response.write("credentials another way: " + credentialsOtherWay + "\n");
