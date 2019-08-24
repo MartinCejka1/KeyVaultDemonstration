87e394c38261b036637d892f48968df50c737c01
Martin-Cejka1 <martin.cejka1@ibm.com>
2019-08-25 01:03:50 +0200

    details WA

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function getSecretValue(){ // returning value of the secret using functions getK
 function useSecret(){                 // Call Watson Assistant, send him message and return answer
     const testApi = new WA({
         version: '2019-02-28',
-        iam_apikey: '6RiEt_DkhzxZO7PdVasxVTdv6KbHYHuA8UpK3V72qJtg', // secret
+        iam_apikey: secretValue,
         url: 'https://gateway-fra.watsonplatform.net/assistant/api'
       });
       testApi.message({
@@ -44,7 +44,7 @@ function useSecret(){                 // Call Watson Assistant, send him message
         input: {'text': 'Jak se mas?'}
         })
         .then(res => {
-          console.log(res);
+          console.log(res.output.text);
           return(JSON.stringify(res, null, 2));
         })
         .catch(err => {
