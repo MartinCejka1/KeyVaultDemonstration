d0b460420c3807b1094e2cb29dd648e19701afa4
Martin-Cejka1 <martin.cejka1@ibm.com>
2019-08-25 00:36:33 +0200

    displaying secret

--- index.js
+++ index.js
@@ -23,44 +23,16 @@ function getKeyVaultSecret(credentials) {
 }
 
 function getSecretValue(){ // returning value of the secret using functions getKeyVaultCredentials and getKeyVaultSecret in asynchronous way
-    getKeyVaultCredentials().then(
-        getKeyVaultSecret
-    ).then(function (secret){
-        secretValue = 'HOUBY';
-        console.log(secret.value);
-        return secretValue;
+    getKeyVaultCredentials().then(      // call for credentials and wait until receive them
+        getKeyVaultSecret               // call for secret and wait until recieve the object
+    ).then(function (secret){ 
+        secretValue = secret.value;     // retrive value of the secret and put it into a global variable
+        console.log(secret.value);      // print into a azure logs (resources --> your app --> log stream)
     }).catch(function (err) {
         throw (err);
     });
 }
-/*
-function getKeyVaultCredentials(){
-    msRestAzure.loginWithAppServiceMSI({resource: 'https://vault.azure.net'}).then((value)=>{console.log(value);return value;});
-}
 
-function getKeyVaultSecret(credentials) {
-    let keyVaultClient = new KeyVault.KeyVaultClient(credentials);
-    var url = getUrl();
-    keyVaultClient.getSecret(url, 'watsonAssistentApikey', "").then((value)=>{return value;});
-  }
-
-function loginTheOtheWay(){
-    let clientId = process.env['CLIENT_ID'] || '8f87c1ea-37a1-4d12-a372-e67de6d7ff45'; // service principal
-    let domain = process.env['DOMAIN']; // tenant id
-    let secret = process.env['APPLICATION_SECRET'] || 'watsonAssistantApikey';
-    if (process.env.APPSETTING_WEBSITE_SITE_NAME){
-      return msRestAzure.loginWithAppServiceMSI();
-    } else {
-      return msRestAzure.loginWithServicePrincipalSecret(clientId, secret, domain);
-    }
-  }
-  
-function getSecretOtherWay(credentialsOtherWay) {
-    let keyVaultClient = new KeyVault.KeyVaultClient(credentialsOtherWay);
-    var url = getUrl();
-    keyVaultClient.getSecret(url, 'watsonAssistentApikey', "").then((value)=>{return value;});
-}
-*/
 function useSecret(){
     const testApi = new WA({
         version: '2019-02-28',
@@ -85,36 +57,15 @@ var server = http.createServer(function(request, response) {    // Create a serv
     var url = "https://" + "cs-keyvaultstorage" + ".vault.azure.net/";      // store URL of Key Vault resource running on Azure
     response.write("Keyvault URL: " + url + "\n");                          // display the URL 
     
-    //var credentials = getKeyVaultCredentials();
-    //var credentialString = JSON.stringify(credentials);
-    //var credentialsOtherWay = loginTheOtheWay();
-    //var credentialsOtherWayString = JSON.stringify(credentialsOtherWay);
-
-    //var secret = getKeyVaultSecret(credentials);
-    //var secretString = JSON.stringify(secret);
-    //var secretOtherWay = getSecretOtherWay(credentialsOtherWay);
-    //var secretOtherWayString = JSON.stringify(secretOtherWay);
-
-    //var secretUsed = useSecret(secret);
-
     // Display environmental variables (environment in Azure should keep these values, localy is set as: LOCAL)
     response.write("MSI_SECRET: " + process.env.MSI_SECRET + "\n");
     response.write("MSI_ENDPOINT: " + process.env.MSI_ENDPOINT + "\n");
 
-    //response.write("credentials: " + credentials + "\n");
-    //response.write("credentials as string: " + credentialString + "\n");
-    //response.write("credentials another way: " + credentialsOtherWay + "\n");
-    //response.write("credentials another way as string: " + credentialsOtherWayString + "\n");
-    var secretFromFunction = getSecretValue();                          // call function that returns the stored secret value
-    response.write("SECRET_VALUE: " + secretFromFunction + "\n");       // display the secret value
-    response.write("Global: " + secretValue + "\n");       // display the secret value
+    getSecretValue();                                            // call function that returns the stored secret value
+    response.write("SECRET_VALUE: " + secretValue + "\n");       // display the secret value
 
     var message = useSecret();                        // call function that uses secret as apikey for WA and returns response
     response.write("MESSAGE FROM WA: " + message + "\n");       // display message from Watson Assistant
-    //response.write("secret as string: " + secretString + "\n");
-    //response.write("secret another way: " + secretOtherWay + "\n");
-    //response.write("secret another way: " + secretOtherWayString + "\n");
-    //response.write("WA returns: " + secretUsed + "\n");
 
     response.end(); //end the response
 });
