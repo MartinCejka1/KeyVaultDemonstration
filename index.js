1b9d9bbeff7abb67cd2a1bfd561660d83ea05a80
Martin-Cejka1 <martin.cejka1@ibm.com>
2019-08-25 14:21:16 +0200

    index.js is finally final :)

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const KeyVault = require('azure-keyvault');     // load package to work with Key
 const msRestAzure = require('ms-rest-azure');   // load package to work with azure account
 
 const WA = require('ibm-watson/assistant/v1');  // load package to work with watson assistant (WA) on ibm cloud
-global.secretValue='';
-global.answer = '';
+global.secretValue='';                          // global variable to keep a secret value (apikey)
+global.answer = '';                             // global variable to keep an answer from WA
 
 function getKeyVaultCredentials(){ // Logs in using envrionmental variables and returns credentials 
     return msRestAzure.loginWithAppServiceMSI({resource: 'https://vault.azure.net'});
@@ -22,26 +22,26 @@ function getKeyVaultSecret(credentials) {
     return keyVaultClient.getSecret('https://cs-keyvaultstorage.vault.azure.net/', 'cs-secret', "");
 }
 
-function getSecretValue(){ // returning value of the secret using functions getKeyVaultCredentials and getKeyVaultSecret in asynchronous way
+function getAndUseSecretValue(){        // Get credentials, get secret using them, get answer from WA using secret  
     getKeyVaultCredentials().then(      // call for credentials and wait until receive them
         getKeyVaultSecret               // call for secret and wait until recieve the object
     ).then(function (secret){ 
         secretValue = secret.value;     // retrive value of the secret and put it into a global variable
         console.log(secret.value);      // print into a azure logs (resources --> your app --> log stream)
-        const testApi = new WA({ 
-          version: '2019-02-28',
+        const testApi = new WA({        // create new instance of watson assistant, as apikey use retrieved secret
+          version: '2019-02-28',        
           iam_apikey: secretValue,
           url: 'https://gateway-fra.watsonplatform.net/assistant/api'
         });
-        testApi.message({
+        testApi.message({               // send message to WA
           workspace_id: '1f6ab258-6b10-4afc-9bc4-1c4564d7f93b',
           input: {'text': 'Jak se mas?'}
           })
           .then(res => {
-            console.log(res.output.text);
-            answer = res.output.text;
+            console.log(res.output.text);   // print the answer of WA
+            answer = res.output.text;       // save the answer into a global variable
           })
-          .catch(err => {
+          .catch(err => {                   // hanndling errors
             return(err);
           });   
     }).catch(function (err) {
@@ -50,27 +50,6 @@ function getSecretValue(){ // returning value of the secret using functions getK
     
 }
 
-/*
-function useSecret(a){                 // Call Watson Assistant, send him message and return answer  
-  console.log("A value of secret in UseSecret() is: ", a);
-  const testApi = new WA({ 
-        version: '2019-02-28',
-        iam_apikey: '6RiEt_DkhzxZO7PdVasxVTdv6KbHYHuA8UpK3V72qJtg',
-        url: 'https://gateway-fra.watsonplatform.net/assistant/api'
-      });
-      testApi.message({
-        workspace_id: '1f6ab258-6b10-4afc-9bc4-1c4564d7f93b',
-        input: {'text': 'Jak se mas?'}
-        })
-        .then(res => {
-          console.log(res.output.text);
-          return(JSON.stringify(res, null, 2));
-        })
-        .catch(err => {
-          return(err);
-        });   
-    }
-*/
 var server = http.createServer(function(request, response) {    // Create a server
     response.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
     
@@ -81,11 +60,11 @@ var server = http.createServer(function(request, response) {    // Create a serv
     response.write("MSI_SECRET: " + process.env.MSI_SECRET + "\n");
     response.write("MSI_ENDPOINT: " + process.env.MSI_ENDPOINT + "\n");
 
-    getSecretValue()                                                 // call function that returns the stored secret value
+    getAndUseSecretValue()                                           // call function to get secret, use it to call WA and store both in global variables
     response.write("SECRET_VALUE: " + secretValue + "\n");           // display the secret value
-    response.write('ANSWER FROM WA: ' + answer + "\n");                        // call function that uses secret as apikey for WA and returns response
+    response.write('ANSWER FROM WA: ' + answer + "\n");              // display the answer of WA
   
-    response.end();                                                 //end the response
+    response.end();                                                 // end the response
 });
  
 var port = process.env.PORT || 1337;                            // set PORT, if not set in .env, use 1337
